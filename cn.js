af6076edbc276a46eeb71839d16c82f5edf12420
Tom <tom@dnomaid.co.uk>
2018-02-14 18:13:22 +0000

    Update cn.js

--- cn.js
+++ cn.js
@@ -1,7 +1,7 @@
 function shuffleArray(array) {
-  for (var i = array.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1));
-    var temp = array[i];
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = array[i];
     array[i] = array[j];
     array[j] = temp;
   }
@@ -16,24 +16,25 @@ function addZero(what) {
   }
 }
 
-var months = [4, 4, 4, 10, 10];
-currentYr = parseInt(new Date().getFullYear().toString().substr(2, 2));
+const months = [4, 4, 4, 10, 10];
+const currentYr = parseInt(new Date().getFullYear().toString().substr(2, 2));
 document.getElementById('year').innerHTML = Math.floor(Math.random() * (currentYr + 5)) + 4;
 document.getElementById('month').innerHTML = addZero(months[Math.floor(Math.random() * months.length)]);
 
+let letter;
 if (window.location.hash) {
   if (window.location.hash.charAt(1) == 'x') {
-    letter = 'y'
+    letter = 'y';
   } else {
     letter = window.location.hash.charAt(1);
   }
 } else {
-  letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   letter = letters[Math.floor(Math.random() * letters.length)];
 }
 
 $.get('animals.txt', function(text) {
-  animals = shuffleArray(text.split('\n'));
+  let animals = shuffleArray(text.split('\n'));
   for (var i = 0; i < animals.length; i++) {
     if (animals[i].charAt(0).toLowerCase() == letter.toLowerCase()) {
       animal = animals[i];
@@ -43,11 +44,11 @@ $.get('animals.txt', function(text) {
   letter = animal.charAt(0);
 
 
-  div = document.getElementById('response');
+  let div = document.getElementById('response');
 
   $.get('adj.txt', function(text) {
 
-    adjs = shuffleArray(text.split('\n'));
+    let adjs = shuffleArray(text.split('\n'));
 
     for (var i = 0; i < adjs.length; i++) {
       if (adjs[i].charAt(0) == letter.toLowerCase()) {
